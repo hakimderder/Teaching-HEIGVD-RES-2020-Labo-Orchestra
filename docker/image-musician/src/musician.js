b67a77b67684f0d4326cd5b8f71a4d30c0c67b62
Hakim Derder <hakderder@gmail.com>
2021-06-19 19:17:14 +0200

    tentative de musician

---
+++ docker/image-musician/src/musician.js
@@ -0,0 +1,35 @@
+// var uuid = require('uuid');
+const {v4: uuidv4} = require ('uuid');
+var dgram = require('dgram');
+var socket = dgram.createSocket('udp4');
+
+const protocol = require('./protocol');
+
+function Musician(instrument, uuid){
+        this.instrument = instrument;
+        this.uuid = uuid;
+
+        var infos = {
+            uuid: this.uuid,
+            sound:protocol[instrument]
+        };
+        var payload = JSON.stringify(infos);
+        var message = Buffer.from(payload)
+
+        Musician.prototype.update = function(){
+            socket.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS,
+                function (error, bytes){
+                console.log("Sending payload: " + payload + " via port " + socket.address().port);
+                });
+        }
+
+        setInterval(this.update.bind(this), 1000);
+}
+
+if(process.argv.length < 3){
+    console.log("Need 3 arguments");
+    process.exit();
+}
+var instrument = process.argv[2];
+
+var musician = new Musician(instrument, uuidv4);
